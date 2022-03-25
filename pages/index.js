64c604bd7c7e36da5d0724d6dff8b284ea7c9665
Iain Johnston <64069140+iain0202@users.noreply.github.com>
2022-03-25 18:10:41 +0800

    second

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Footer from '../comps/Footer'
-import Navbar from '../comps/navbar'
+import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
